04b12e14297c78ee5e4639bf42d05d2da02b5ca4
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-06-30 13:46:28 +0200

    generics fixs

--- src/main/java/it/growbit/matlab/controller/MatlabRestController.java
+++ src/main/java/it/growbit/matlab/controller/MatlabRestController.java
@@ -1,16 +1,10 @@
 package it.growbit.matlab.controller;
 
-import com.mathworks.toolbox.javabuilder.MWException;
 import it.growbit.matlab.model.Last24HoursAvg;
 import it.growbit.matlab.model.Next24HourAvg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by name on 24/06/17.
@@ -62,11 +56,11 @@ public class MatlabRestController {
         return next24houravg;
     }
 
-    @RequestMapping(value = "/matlab/{package}/{method}", method = RequestMethod.POST)
+    @RequestMapping(value = "/matlab/{packageName}/{methodName}", method = RequestMethod.POST)
     public Next24HourAvg matlab_generics(
             @RequestBody Last24HoursAvg matlab_payload,
-            @PathParam("package") String packageName,
-            @PathParam("method") String methodName
+            @PathVariable(value="packageName") String packageName,
+            @PathVariable(value="methodName") String methodName
     ) throws Exception {
 
         Double forecast = this.matlab.generic_invocation(packageName, methodName, matlab_payload);
